55ccbcfc3270eaf259d75ba864afe89d58bca9fc
Santiago Alarcon <santiago-alar@hotmail.com>
2021-05-21 18:25:21 -0500

    Pares e impares con IntPredicate

---
+++ src/package1/FifthChallenge.java
@@ -0,0 +1,25 @@
+//5)Muestra los números impares y pares del 1 al 100 (ambos incluidos). Usa un
+//bucle while.
+package package1;
+
+import java.util.function.IntPredicate;
+
+public class FifthChallenge {
+    public static void main(String[] args) {
+        int number = 1;
+
+        while (number<=10){
+            if (condition(number)) {
+                System.out.println(number + " is even");
+            } else {
+                System.out.println(number + " is even");
+            }
+            number++;
+        }
+    }
+    
+    public static boolean condition(int number){
+        IntPredicate isEven = x-> x%2 ==0;
+        return isEven.test(number);
+    }
+}
