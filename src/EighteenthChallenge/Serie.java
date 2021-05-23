a5c4ee89831934e0c35e3fdc89d3191dcbef1d95
Santiago Alarcon <santiago-alar@hotmail.com>
2021-05-23 14:20:26 -0500

    Create Serie class for the 18th Challenge

---
+++ src/EighteenthChallenge/Serie.java
@@ -0,0 +1,84 @@
+package EighteenthChallenge;
+
+public class Serie {
+    //•	Sus atributos son título, numero de temporadas, entregado, género y creador.
+    //•	Por defecto, el número de temporadas es de 3 temporadas y entregado false.
+    // El resto de atributos serán valores por defecto según el tipo del atributo.
+
+    //Attributes
+    String titulo = "";
+    int numTemp = 3;
+    boolean entregado = false;
+    String genero = "";
+    String creador = "";
+
+    //Constructors
+    //•	Los constructores que se implementarán serán:
+    //•	Un constructor por defecto.
+    //•	Un constructor con el titulo y creador. El resto por defecto.
+    //•	Un constructor con todos los atributos, excepto de entregado.
+
+    public Serie() {
+    }
+
+    public Serie(String titulo, String creador) {
+        this.titulo = titulo;
+        this.creador = creador;
+    }
+
+    public Serie(String titulo, int numTemp, String genero, String creador) {
+        this.titulo = titulo;
+        this.numTemp = numTemp;
+        this.genero = genero;
+        this.creador = creador;
+    }
+
+    //•	Los métodos que se implementara serán:
+    //•	Métodos get de todos los atributos, excepto de entregado.
+    //•	Métodos set de todos los atributos, excepto de entregado.
+    //•	Sobrescribe los métodos toString.
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getNumTemp() {
+        return numTemp;
+    }
+
+    public void setNumTemp(int numTemp) {
+        this.numTemp = numTemp;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCreador() {
+        return creador;
+    }
+
+    public void setCreador(String creador) {
+        this.creador = creador;
+    }
+
+    @Override
+    public String toString() {
+        return "Serie{" +
+                "titulo='" + titulo + '\'' +
+                ", numTemp=" + numTemp +
+                ", entregado=" + entregado +
+                ", genero='" + genero + '\'' +
+                ", creador='" + creador + '\'' +
+                '}';
+    }
+}
