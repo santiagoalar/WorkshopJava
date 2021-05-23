61fc83cc8c1f1d83c8edc1e324a74e50f68015c8
Santiago Alarcon <santiago-alar@hotmail.com>
2021-05-22 22:33:56 -0500

    Create date formatter (yyyy/MM/dd) (HH:mm:ss) for 13th challenge

---
+++ src/package1/ThirteenthChallenge.java
@@ -0,0 +1,16 @@
+//Realizar un algoritmo que permita consultar la fecha y hora actual en el
+//formato (AAAA/MM/DD) (HH:MM:SS)
+package package1;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ThirteenthChallenge {
+    public static void main(String[] args) {
+        LocalDateTime actualTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("(yyyy/MM/dd) (HH:mm:ss)");
+        System.out.println("The actual time is: ");
+        System.out.print(formatter.format(actualTime));
+    }
+}
