b0d73e6340fd10403f6bd677f50bb5da6aaf1481
Santiago Alarcon <santiago-alar@hotmail.com>
2021-05-27 15:38:11 -0500

    Formatting code and converting methods to functional

--- src/package1/NinthChallenge.java
+++ src/package1/NinthChallenge.java
@@ -4,26 +4,34 @@
 //luego unidas.
 package package1;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class NinthChallenge {
     public static void main(String[] args) {
-        final String PHRASE = " La sonrisa sera la mejor arma contra la tristeza";
-        String newPhrase = "";
-        for (int i = 0; i < PHRASE.length(); i++) {
-            if (PHRASE.charAt(i) == 'a') {
-                newPhrase += "e";
-            }else {
-                newPhrase += PHRASE.charAt(i);
-            }
-        }
-        concatenateNewPhrase(newPhrase);
-    }
-
-    public static void concatenateNewPhrase(String phraseWithE){
+        String phrase = " La sonrisa sera la mejor arma contra la tristeza";
+        List<Character> listString = convertStringToCharList(phrase);
         Scanner entry = new Scanner(System.in);
         System.out.println("Enter a phrase in order to concatenate to saved phrase in our system: ");
         String phraseToPrint = entry.nextLine();
-        System.out.println(phraseToPrint.concat(phraseWithE));
+
+        convertOnlyA(listString, phraseToPrint);
+
+    }
+
+    public static void convertOnlyA(List<Character> listString, String stringToConcatenate) {
+        List<Character> newList = listString.stream().map(x -> (x == 'a') ? 'e' : x).collect(Collectors.toList());
+        concatenateNewPhrase(stringToConcatenate, newList);
+    }
+
+    public static void concatenateNewPhrase(String phraseWithE, List<Character> newList) {
+        newList.stream().forEach(System.out::print);
+        System.out.print(" " + phraseWithE);
+    }
+
+    public static List<Character> convertStringToCharList(String str) {
+        List<Character> chars = str.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+        return chars;
     }
 }
