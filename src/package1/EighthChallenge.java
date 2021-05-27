979e1ebc94db97f445b298de13c0c85ac3494858
Santiago Alarcon <santiago-alar@hotmail.com>
2021-05-27 14:22:28 -0500

    Change method to functional one

--- src/package1/EighthChallenge.java
+++ src/package1/EighthChallenge.java
@@ -2,7 +2,7 @@
 //diga si es un día laboral o no. Usa un switch para ello.
 package package1;
 
-import java.util.Locale;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EighthChallenge {
@@ -14,39 +14,19 @@ public class EighthChallenge {
         checkDate(dateEnter);
     }
 
-    public static void checkDate(String date){
-        switch (date){
-            case "lunes":
-                isWeekDay(date, true);
-                break;
-            case "martes":
-                isWeekDay(date,true);
-                break;
-            case "miercoles":
-                isWeekDay(date, true);
-                break;
-            case "jueves":
-                isWeekDay(date,true);
-                break;
-            case "viernes":
-                isWeekDay(date, true);
-                break;
-            case "sabado":
-                isWeekDay(date, false);
-                break;
-            case "domingo":
-                isWeekDay(date, false);
-                break;
-            default:
-                System.out.println("Please, enter a correct value.");
-        }
+    public static void checkDate(String date) {
+        String[] WEEKDAYS = {"lunes", "martes", "miercoles", "miércoles", "jueves", "viernes"};
+        boolean isWeekDay = Arrays.stream(WEEKDAYS).anyMatch(x -> x.equalsIgnoreCase(date));
+        checkWeekDay(date, isWeekDay);
     }
 
-    public static void isWeekDay(String day, boolean isweekday){
-        if(isweekday) {
+    public static void checkWeekDay(String day, boolean isWeekday) {
+        if (isWeekday) {
             System.out.println(day + " is a weekday, time to go back to work");
-        }else {
+        } else {
             System.out.println(day + " is a weekend day, let's go to the party!");
         }
     }
+
+
 }
